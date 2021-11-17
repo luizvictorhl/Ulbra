7e6b42b17189a8f757e7218514e951508ceea1e9
Luiz Victor Hoffmann Likoski <luizvictorcprs@gmail.com>
2021-11-16 21:45:48 -0300

    edit-codigos

--- 2021.2/LABORATÓRIO DE PROGRAMAÇÃO/Exercicios/src/AS/Main.java
+++ 2021.2/LABORATÓRIO DE PROGRAMAÇÃO/Exercicios/src/AS/Main.java
@@ -19,9 +19,10 @@ import java.util.Scanner;
                 System.out.println("Digite 1 para ADICIONAR séries e seus dados;");
                 System.out.println("Digite 2 LISTAR as séries vistas e não vistas;");
                 System.out.println("Digite 3 para REMOVER uma série por seu nome;");
-                System.out.println("Digite 4 para ENCONTRAR uma série;");
-                System.out.println("Digite 5 MOSTRAR a QUANTIDADE total de Séries no catálogo;");
-                System.out.println("Digite 6 para sair do programa.");
+                System.out.println("Digite 4 para REMOVER uma série por sua posição;");
+                System.out.println("Digite 5 para ENCONTRAR uma série;");
+                System.out.println("Digite 6 MOSTRAR a QUANTIDADE total de Séries no catálogo;");
+                System.out.println("Digite 7 para sair do programa.");
                 op = tc.nextInt();
                 System.out.println("");
                 switch (op) {
@@ -33,45 +34,67 @@ import java.util.Scanner;
                     }
 
                     case 2: {
-                        System.out.println("\nSéries visualizadas:");
-                        for (int i = 0; i < catalogo.size(); i++) {
-                            catalogo.get(i).serieVisualizada();
-                        }
-                        System.out.println("\nSéries não visualizadas:");
-                        for (int i = 0; i < catalogo.size(); i++) {
-                            catalogo.get(i).serieNaoVisualizada();
+                        if (catalogo.isEmpty()) {
+                            System.out.println("Nenhuma série está cadastrada no momento!");
+                        } else {
+                            System.out.println("\nSéries visualizadas:");
+                            for (int i = 0; i < catalogo.size(); i++) {
+                                catalogo.get(i).serieVisualizada();
+                            }
+                            System.out.println("\nSéries não visualizadas:");
+                            for (int i = 0; i < catalogo.size(); i++) {
+                                catalogo.get(i).serieNaoVisualizada();
+                            }
+
                         }
                         break;
                     }
-
                     case 3: {
-                        System.out.println("Digite a posição a ser apagada:");
-                        int posicao = tc.nextInt();
-                        Serie apagar = catalogo.remove(posicao);
-                        if (apagar != null) {
-                            System.out.println("Livro apagado: " + apagar.titulo);
-                        } else {
-                            System.out.println("Nao encontrei");
+                        System.out.println("Digite o nome da série que deseja apagar:");
+                        String nome = tc.next();
+                        Serie apagar = new Serie(nome);
+
+                        if(catalogo.remove(apagar)){
+                            System.out.println("Removido!\nA série removida foi: " + nome);
+                        }else{
+                            System.out.println("Não encontrei a série no catálogo.");
                         }
                         break;
                     }
 
                     case 4: {
-                        System.out.println("Digite o nome da série a ser buscada: ");
-                        String nome = tc.next();
-
-                        Serie buscar = new Serie(nome);
-
-                        if (catalogo.contains(buscar)) {
-                            System.out.println("\nEncontrei a série " + nome + " na posiçao:" + catalogo.indexOf(buscar));
-                            catalogo.get(catalogo.indexOf(buscar)).exibirDados();
+                        System.out.println("Digite a posição a ser apagada:");
+                        int posicao = tc.nextInt();
+                        Serie apagar = catalogo.remove(posicao);
+                        if (apagar != null) {
+                            System.out.println("Série apagada: " + apagar.titulo);
                         } else {
-                            System.out.println("\nDesculpe, não encontrei a série desejada.");
+                            System.out.println("Posição não registra série cadastrada no momento!");
                         }
                         break;
                     }
 
                     case 5: {
+                        if (catalogo.isEmpty()) {
+                            System.out.println("Catalogo vazio!");
+                        } else {
+                            System.out.println("Digite o nome da série a ser buscada: ");
+                            String nome = tc.next();
+
+                            Serie buscar = new Serie(nome);
+
+                            if (catalogo.contains(buscar)) {
+                                System.out.println("\nEncontrei a série " + nome + " na posiçao:" + catalogo.indexOf(buscar));
+                                catalogo.get(catalogo.indexOf(buscar)).exibirDados();
+                            } else {
+                                System.out.println("\nDesculpe, não encontrei a série desejada.");
+                            }
+
+                        }
+                        break;
+                    }
+
+                    case 6: {
                         System.out.println("Total de séries cadastradas: " + catalogo.size());
                         for (int i = 0; i < catalogo.size(); i++) {
                             catalogo.get(i).exibirDados();
@@ -80,7 +103,7 @@ import java.util.Scanner;
                     }
                 }
 
-            }while (op != 6) ;
+            }while (op != 7) ;
                     System.out.println("Obrigado por utilizar!");
                 }
 
