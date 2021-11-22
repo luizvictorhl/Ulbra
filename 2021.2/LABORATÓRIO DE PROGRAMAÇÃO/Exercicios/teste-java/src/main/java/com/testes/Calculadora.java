e07e5a17b576d8ceca11efb44d19c33974c9e9b8
Luiz Victor Hoffmann Likoski <luizvictorcprs@gmail.com>
2021-11-22 12:15:08 -0300

    atualizacoes_0.1

---
+++ 2021.2/LABORATÓRIO DE PROGRAMAÇÃO/Exercicios/teste-java/src/main/java/com/testes/Calculadora.java
@@ -0,0 +1,65 @@
+package com.testes;
+
+
+
+import java.util.Scanner;
+
+public class Calculadora {
+    Scanner ler = new Scanner(System.in);
+    String nome;
+
+    public void boasVindas(){
+        System.out.println("Qual seu nome?");
+        String nome = ler.nextLine();
+        System.out.println("Seja bem vindo(a) " + nome + "!");
+    }
+
+    double c;
+    public void somar(){
+        System.out.println("\nDigite o valor do primeiro número:");
+        double a = ler.nextDouble();
+        System.out.println("Digite o valor do segundo número:");
+        double b = ler.nextDouble();
+        c = a + b;
+        System.out.println("\nO resultado do cálculo foi: " + c);
+    }
+    public int som2(int a, int b){
+        return a + b;
+    }
+
+    public void subtrair(){
+        System.out.println("\nDigite o valor do primeiro número:");
+        double a = ler.nextDouble();
+        System.out.println("Digite o valor do segundo número:");
+        double b = ler.nextDouble();
+        c = a - b;
+        System.out.println("\nO resultado do cálculo foi: " + c);
+    }
+    public int sub2(int a, int b){
+        return a - b;
+    }
+
+    public void multiplicar(){
+        System.out.println("\nDigite o valor do primeiro número:");
+        double a = ler.nextDouble();
+        System.out.println("Digite o valor do segundo número:");
+        double b = ler.nextDouble();
+        c = a * b;
+        System.out.println("\nO resultado do cálculo foi: " + c);
+    }
+    public int mult2(int a, int b){
+        return a * b;
+    }
+
+    public void dividir(){
+        System.out.println("\nDigite o valor do primeiro número:");
+        double a = ler.nextDouble();
+        System.out.println("Digite o valor do segundo número:");
+        double b = ler.nextDouble();
+        c = a / b;
+        System.out.println("\nO resultado do cálculo foi: " + c);
+    }
+    public int div2(int a, int b){
+        return a / b;
+    }
+}
---
+++ 2021.2/LABORATÓRIO DE PROGRAMAÇÃO/Exercicios/teste-java/src/test/java/com/testes/TestesCalculator.java
@@ -0,0 +1,38 @@
+package com.testes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestesCalculator {
+    @Test
+    public void testReturnSom2(){
+        Calculadora cal = new Calculadora();
+        assertEquals(15, cal.som2(9,5)+1);
+    }
+
+    @Test
+    public void testReturnSub2(){
+        Calculadora cal = new Calculadora();
+        assertEquals(5, cal.sub2(10,4));
+    }
+
+    @Test
+    public void testReturnMult2(){
+        Calculadora cal = new Calculadora();
+        assertEquals(20, cal.mult2(10,5));
+    }
+
+    @Test
+    public void testReturnZero(){
+        Calculadora cal = new Calculadora();
+        assertEquals(0, cal.mult2(10,0));
+    }
+
+    @Test
+    public void testReturnDiv2(){
+        Calculadora cal = new Calculadora();
+        assertEquals(2, cal.div2(10,5));
+    }
+
+}
