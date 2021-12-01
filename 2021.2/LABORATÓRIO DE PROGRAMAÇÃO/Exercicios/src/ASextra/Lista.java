e9d6fb41f8bcd842be682de4ce7c5464af0fc53f
Luiz Victor Hoffmann Likoski <luizvictorcprs@gmail.com>
2021-11-30 21:26:31 -0300

    alteracoes

--- 2021.2/LABORATÓRIO DE PROGRAMAÇÃO/Exercicios/src/ASextra/Lista.java
+++ 2021.2/LABORATÓRIO DE PROGRAMAÇÃO/Exercicios/src/ASextra/Lista.java
@@ -1,2 +1,151 @@
-package ASextra;public class Lista {
+package ASextra;
+
+import java.util.ArrayList;
+
+public class Lista {
+    public Cao primeiro;
+    public int tam;
+    public Cao ultimo;
+
+    public int size() {
+        return tam;
+    }
+
+    @Override
+    public String toString() {
+        return "Lista{" +
+                "primeiro=" + primeiro +
+                ", tam=" + tam +
+                '}';
+    }
+
+    public boolean isEmpty() {
+        if (primeiro == null) {
+            return true;
+        } else {
+            return false;
+        }
+
+        /*
+        OU
+        if(tam == 0){
+            return true;
+        }else{
+            return false;
+        }*/
+    }
+
+    public boolean add(Cao caozinho) {
+        if (this.primeiro == null) {
+            this.primeiro = caozinho;
+            this.tam++;
+            return true;
+        } else {
+            caozinho.proximo = primeiro;
+            primeiro = caozinho;
+            this.tam++;
+            return true;
+        }
+    }
+
+    public boolean addFim(Cao caozinho){
+        if(this.primeiro==null){
+            return add(caozinho);
+        }else{
+            Cao aux=primeiro;
+
+            while (aux.proximo!=null){
+                aux=aux.proximo;
+            }
+            aux.proximo=caozinho;
+            this.ultimo=caozinho;
+            tam++;
+            return true;
+        }
+    }
+
+    public void exibir(){
+        if(isEmpty()){
+            System.out.println("Lista vazia!");
+        }else {
+            Cao aux = primeiro;
+            aux.exibir();
+            while (aux.proximo != null) {
+                aux = aux.proximo;
+                aux.exibir();
+            }
+        }
+    }
+
+    public boolean remover(){
+        if (!isEmpty()){
+            Cao p =this.primeiro;
+            this.primeiro=p.proximo;
+            this.tam--;
+            return true;
+            //   this.primeiro=this.primeiro.proximo;
+        }else{
+            System.out.println("Lista vazia");
+            return false;
+        }
+    }
+
+    //remover em uma posição
+    public boolean remover(int x){
+        if(x == 0){
+            return this.remover();
+        }else if(x > tam){
+            return false;
+        }else if(x==tam-1){
+            return this.removerFim();
+        }else{
+            int count = 0;
+            Cao elemento = this.primeiro;
+            Cao anterior = this.primeiro;
+            while(x==count){
+                count ++;
+                anterior = elemento;
+                elemento = elemento.proximo;
+            }
+            anterior.proximo = elemento.proximo;
+            this.tam--;
+            return true;
+        }
+    }
+
+    public boolean removerFim(){
+        if(this.primeiro == null){
+            System.out.println("lista vazia");
+            return false;
+        }else {
+            Cao aux = primeiro;
+            Cao penultimo = primeiro;
+            while (aux.proximo != null) {
+                penultimo = aux;
+                aux = aux.proximo;
+            }
+
+            penultimo.proximo = null;
+            this.ultimo = penultimo;
+            this.tam--;
+            return true;
+        }
+    }
+
+    //REMOVER ELEMENTO POR NOME:
+    public boolean removerNome(String nomeP){
+        if(isEmpty()){
+            System.out.println("Lista vazia!");
+            return false;
+        }else {
+            Cao aux = primeiro;
+            int i = 0;
+
+            while (!aux.nome.equals(nomeP)) {
+                i++;
+                aux = aux.proximo;
+            }
+            return this.remover(i);
+        }
+    }
 }
--- 2021.2/LABORATÓRIO DE PROGRAMAÇÃO/Exercicios/src/ASextra/Main.java
+++ 2021.2/LABORATÓRIO DE PROGRAMAÇÃO/Exercicios/src/ASextra/Main.java
@@ -1,2 +1,33 @@
-package ASextra;public class Main {
+package ASextra;
+
+public class Main {
+    public static void main(String[] args) {
+        Lista canil = new Lista();
+
+        //adicionar cachorrinhos no canil
+        for(int i = 0 ; i < 3; i++){
+            Cao cao = new Cao();
+            cao.ler();
+            canil.addFim(cao);
+        }
+
+        System.out.println(canil.toString());
+
+        System.out.println("Quantos elementos ele tem: " + canil.size());
+        if(canil.isEmpty()){
+            System.out.println("Vazia");
+        }else{
+            System.out.println("Cheia");
+        }
+
+        /*canil.exibir();
+        System.out.println("remover");
+        canil.remover(2);*/
+
+        //REMOVER POR NOME:
+        canil.exibir();
+        canil.removerNome("auau");
+        System.out.println("\nApós exclusão:");
+        canil.exibir();
+    }
 }
