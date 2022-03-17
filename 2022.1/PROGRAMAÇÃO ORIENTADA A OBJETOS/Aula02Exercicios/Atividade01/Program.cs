f47a0acb2c5968fa583c99f0553018de27bc2a21
Luiz Victor Hoffmann Likoski <luizvictorcprs@gmail.com>
2022-03-16 21:14:24 -0300

    erros

--- 2022.1/PROGRAMAÇÃO ORIENTADA A OBJETOS/Aula02Exercicios/Atividade01/Program.cs
+++ 2022.1/PROGRAMAÇÃO ORIENTADA A OBJETOS/Aula02Exercicios/Atividade01/Program.cs
@@ -1,18 +1,21 @@
 ﻿using System;
+usign Atividade01.Entitie;
 
-namespace Atividade01
+namespace Atividade01;
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("CALCULO DE ÁREA DO TRIÂNGULO");
+                
                 AreaTriangulo objcalculo = new AreaTriangulo();
                 objcalculo.SetBaseT(20);
                 objcalculo.SetAlturaT(100);
-                Console.WriteLine("Base do Triângulo " + objcalculo.GetBaseT())
-                Console.WriteLine("Altura do Triângulo " + objcalculo.GetAlturaT())
-                Console.WriteLine("Área do Triângulo " + objcalculo.AreaT())
+                Console.WriteLine("Base do Triângulo " + objcalculo.GetBaseT());
+                Console.WriteLine("Altura do Triângulo " + objcalculo.GetAlturaT());
+                Console.WriteLine("Área do Triângulo " + objcalculo.AreaT());
+
 
         }
     }
