00a7684c7b389301a6a27261151106af84791264
luizvictorhl <luizvictorcprs@gmail.com>
2023-10-31 19:48:31 -0300

    upload_endOctober

---
+++ 2023.2/BD/aulabd/main.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+mongoose.connect("mongodb://127.0.0.1:27017/myapp", {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+const itemSchema = new Schema({
+nome: String,
+descricao: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.use(express.json());
+app.post('/items', async (req, res) => {
+try {
+const newItem = new Item(req.body);
+const savedItem = await newItem.save();
+res.json(savedItem);
+} catch (error) {
+res.status(500).json({ error: 'Não foi possível salvar o item.' });
+}
+});
+
+app.get('/items', async (req, res) => {
+try {
+const items = await Item.find();
+res.json(items);
+} catch (error) {
+res.status(500).json({ error: 'Não foi possível obter os itens.' });
+}
+});
+
+app.listen(port, () => {
+console.log(`Servidor está rodando em http://localhost:${port}`);
+});
