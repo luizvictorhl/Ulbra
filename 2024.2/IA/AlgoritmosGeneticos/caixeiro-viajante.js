d6c3e141648273cb07975e2d8cec1408ad38315d
Luiz <luizvictorcprs@gmail.com>
2024-10-30 10:42:52 -0300

    i.a

--- 2024.2/IA/AlgoritmosGeneticos/caixeiro-viajante.js
+++ 2024.2/IA/AlgoritmosGeneticos/caixeiro-viajante.js
@@ -1,14 +1,18 @@
 const distancias = [
-    // Matriz de distâncias entre as cidades (5 cidades)
-    [0, 2, 4, 3, 6],  // Cidades 1 a outras
-    [2, 0, 4, 3, 7],  // Cidades 2 a outras
-    [4, 4, 0, 3, 3],  // Cidades 3 a outras
-    [3, 3, 3, 0, 3],  // Cidades 4 a outras
-    [6, 7, 3, 3, 0]   // Cidades 5 a outras
+    [0, 2, 4, 3, 6, 8, 3, 5, 7, 9],  // Cidades 1 a outras
+    [2, 0, 4, 3, 7, 6, 5, 4, 8, 2],  // Cidades 2 a outras
+    [4, 4, 0, 3, 3, 5, 6, 2, 1, 7],  // Cidades 3 a outras
+    [3, 3, 3, 0, 3, 6, 4, 8, 9, 2],  // Cidades 4 a outras
+    [6, 7, 3, 3, 0, 5, 6, 4, 1, 3],  // Cidades 5 a outras
+    [8, 6, 5, 6, 5, 0, 7, 8, 9, 4],  // Cidades 6 a outras
+    [3, 5, 6, 4, 6, 7, 0, 2, 3, 5],  // Cidades 7 a outras
+    [5, 4, 2, 8, 4, 8, 2, 0, 6, 7],  // Cidades 8 a outras
+    [7, 8, 1, 9, 1, 9, 3, 6, 0, 2],  // Cidades 9 a outras
+    [9, 2, 7, 2, 3, 4, 5, 7, 2, 0]   // Cidades 10 a outras
 ];
 
 const tamanhoPopulacao = 200;
-const mutationRate = 0.01; // Taxa de mutação
+const mutationRate = 0.01;
 const geracoes = 10000;
 
 function calcularDistanciaTotal(percurso) {
@@ -16,13 +20,12 @@ function calcularDistanciaTotal(percurso) {
     for (let i = 0; i < percurso.length - 1; i++) {
         distanciaTotal += distancias[percurso[i] - 1][percurso[i + 1] - 1];
     }
-    // Retornar à cidade inicial
     distanciaTotal += distancias[percurso[percurso.length - 1] - 1][percurso[0] - 1];
     return distanciaTotal;
 }
 
 function criarIndividuo() {
-    const individuo = [1, 2, 3, 4, 5]; // Cidades 1 a 5
+    const individuo = Array.from({ length: distancias.length }, (_, i) => i + 1);
     for (let i = individuo.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [individuo[i], individuo[j]] = [individuo[j], individuo[i]];
@@ -43,12 +46,11 @@ function aptidao(individuo) {
 }
 
 function selecao(populacao) {
-    const torneio = [];
-    for (let i = 0; i < 5; i++) {
-        torneio.push(escolhaAleatoria(populacao));
+    const torneio = new Set();
+    while (torneio.size < 5) {
+        torneio.add(escolhaAleatoria(populacao));
     }
-    torneio.sort((a, b) => aptidao(b) - aptidao(a));
-    return torneio[0];
+    return [...torneio].sort((a, b) => aptidao(b) - aptidao(a))[0];
 }
 
 function cruzamento(pai1, pai2) {
@@ -105,6 +107,12 @@ function algoritmoGenetico() {
             console.log(`Geração: ${geracao}, Melhor distância: ${melhorDistancia}`);
         }
 
+    
+        if (melhorDistancia === 25) {
+            console.log(`Melhor percurso encontrado na geração ${geracao}`);
+            break;
+        }
+
         const novaPopulacao = [];
 
         while (novaPopulacao.length < tamanhoPopulacao) {
@@ -118,7 +126,8 @@ function algoritmoGenetico() {
         populacao = novaPopulacao;
     }
 
-    console.log(`Melhor percurso final: ${melhorIndividuo.join(' -> ')}`);
+    const percursoFinal = [...melhorIndividuo, melhorIndividuo[0]];
+    console.log(`Melhor percurso final: ${percursoFinal.join(' -> ')}`);
     console.log(`Distância final: ${melhorDistancia}`);
 }
 
